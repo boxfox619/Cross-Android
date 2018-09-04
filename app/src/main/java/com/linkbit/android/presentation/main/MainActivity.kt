5b52c33311bdb50b152fe5708847f40f71ce08c3
Ashton Yoon <y109384@naver.com>
2018-09-04 20:59:31 +0900

    fixed invalid ui statistic card, apply light theme status bar

--- app/src/main/java/com/linkbit/android/presentation/main/MainActivity.kt
+++ app/src/main/java/com/linkbit/android/presentation/main/MainActivity.kt
@@ -2,7 +2,10 @@ package com.linkbit.android.presentation.main
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import com.linkbit.android.R
 import com.linkbit.android.data.model.CoinStatistic
 import com.linkbit.android.presentation.BaseActivity
@@ -12,6 +15,10 @@ import com.linkbit.android.presentation.wallet.list.WalletListFragment
 import com.linkbit.android.presentation.wallet.manage.CreateWalletActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.layout_header.*
+import com.linkbit.android.R.id.view
+import android.view.View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+
 
 class MainActivity : BaseActivity<MainActivityPresenter>(), MainActivityView {
 
@@ -21,10 +28,21 @@ class MainActivity : BaseActivity<MainActivityPresenter>(), MainActivityView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // set light theme to status bar
+        var view = window.decorView
+        var flags = view.systemUiVisibility
+        flags = flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        view.systemUiVisibility = flags
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = Color.WHITE
+        }
+
         this.coinStatisticAdapter = CoinStatisticAdapter(this)
         recyclerview_main_coin_statistics.adapter = coinStatisticAdapter
         btn_main_create_wallet.setOnClickListener{startActivity(Intent(this, CreateWalletActivity::class.java))}
         tab_host.setup()
+
         val manager = fragmentManager
         manager.beginTransaction().replace(R.id.tab_wallet_content, WalletListFragment.newInstance()).commit()
         manager.beginTransaction().replace(R.id.tab_friend_content, FriendListFragment.newInstance()).commit()
