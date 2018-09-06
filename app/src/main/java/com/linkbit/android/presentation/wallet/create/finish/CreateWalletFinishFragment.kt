da1fd1e10b319cab6e789ef8d7a3bf9403045da3
Boxfox <boxfoxsg619@gmail.com>
2018-09-07 01:20:19 +0900

    Fix create wallet finish step

--- app/src/main/java/com/linkbit/android/presentation/wallet/create/finish/CreateWalletFinishFragment.kt
+++ app/src/main/java/com/linkbit/android/presentation/wallet/create/finish/CreateWalletFinishFragment.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.linkbit.android.R
 import com.linkbit.android.helper.URLHelper
 import com.linkbit.android.entity.WalletModel
+import com.linkbit.android.presentation.coin.list.CoinListViewHolder
 
 class CreateWalletFinishFragment : Fragment() {
     lateinit var walletModel: WalletModel
@@ -17,10 +19,12 @@ class CreateWalletFinishFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_create_wallet_finish, container, false)
-        Glide.with(activity!!).load(URLHelper.createAssetUrl(activity!!, this.walletModel.coinSymbol.toUpperCase())).into(view.findViewById(R.id.iv_wallet_icon))
-        view.findViewById<TextView>(R.id.tv_wallet_symbol).text = this.walletModel.coinSymbol
-        view.findViewById<TextView>(R.id.tv_wallet_cross_address).text = this.walletModel.linkbitAddress
-        view.findViewById<TextView>(R.id.tv_wallet_address).text = this.walletModel.accountAddress
+        Glide.with(activity!!).load(URLHelper.createAssetUrl(activity!!, this.walletModel.coinSymbol.toUpperCase())).into(view.findViewById(R.id.iv_wallet_symbol_icon))
+        view.findViewById<TextView>(R.id.tv_wallet_info_symbol).text = this.walletModel.coinSymbol
+        view.findViewById<TextView>(R.id.tv_wallet_info_linked_address).text = this.walletModel.linkbitAddress
+        view.findViewById<TextView>(R.id.tv_wallet_info_address).text = this.walletModel.accountAddress
+        view.findViewById<TextView>(R.id.tv_create_wallet_finish_name).text = this.walletModel.walletName
+        view.findViewById<TextView>(R.id.tv_create_wallet_finish_desc).text = this.walletModel.description
         return view
     }
 
