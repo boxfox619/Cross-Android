85f6617cd39f4fd3ed0936b4f82fcafe63671692
boxfox619 <boxfoxsg619@gmail.com>
2018-10-14 04:06:14 +0900

    Implement the set pin activity setting shared preferences

--- app/src/main/java/com/linkbit/android/presentation/view/pin/SetPinActivity.kt
+++ app/src/main/java/com/linkbit/android/presentation/view/pin/SetPinActivity.kt
@@ -1,13 +1,14 @@
 package com.linkbit.android.presentation.view.pin
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
 import com.linkbit.android.R
 import kotlinx.android.synthetic.main.activity_set_pin.*
 
 
 class SetPinActivity : AppCompatActivity() {
-    private var pin: List<Int>? = null
+    private var pin: ArrayList<Int>? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +20,15 @@ class SetPinActivity : AppCompatActivity() {
                 pin = pinNumbers
                 pinInputView.setMessage(getString(R.string.msg_request_password_confirm))
                 pinInputView.reset()
-            } else if (pin.toString() != pinNumbers.toArray().joinToString("")) {
+            } else if (pin!!.joinToString("") != pinNumbers.joinToString("")) {
                 pin = null
                 pinInputView.setErrorMessage(getString(R.string.err_pin_not_match))
                 pinInputView.reset()
             } else {
+                val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+                val editor = prefs.edit()
+                editor.putString("pin", pin!!.joinToString(""))
+                editor.commit()
                 finish()
             }
         }
