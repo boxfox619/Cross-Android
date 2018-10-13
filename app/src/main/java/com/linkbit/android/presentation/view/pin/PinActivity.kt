7a6d92c4cd78b6b8f1af54a61ddbb812953d4cf5
boxfox619 <boxfoxsg619@gmail.com>
2018-10-14 01:32:53 +0900

    Finish fix the pin activty

--- app/src/main/java/com/linkbit/android/presentation/view/pin/PinActivity.kt
+++ app/src/main/java/com/linkbit/android/presentation/view/pin/PinActivity.kt
@@ -1,31 +1,29 @@
 package com.linkbit.android.presentation.view.pin
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.linkbit.android.R
 import kotlinx.android.synthetic.main.activity_set_pin.*
 
 
-class SetPinActivity : AppCompatActivity() {
-    private var pin: List<Int>? = null
-
+class PinActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_set_pin)
+        val correctPin = intent.getStringExtra("pin")
         val pinInputView = (pin_input_view as PinInputView)
         pinInputView.asObservable().subscribe { pinNumbers ->
-            if (pin == null) {
-                pin = pinNumbers
-                pinInputView.setMessage(getString(R.string.msg_request_password_confirm))
-                pinInputView.reset()
-            } else if (pin.toString() != pinNumbers.toString()) {
-                pin = null
+            if (correctPin == pinNumbers.toString()) {
                 pinInputView.setErrorMessage(getString(R.string.err_pin_not_match))
                 pinInputView.reset()
             } else {
+                setResult(Activity.RESULT_OK)
                 finish()
             }
         }
+        setResult(Activity.RESULT_CANCELED)
     }
 }
